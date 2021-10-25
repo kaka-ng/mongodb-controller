f37344ad523347986022320809153f593745dd11
KaKa <kaka@kakawebsitedemo.com>
2021-10-25 19:52:04 +0800

    build: mark external deps

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,15 @@ export default [
     output: [
       { dir: 'lib', format: 'cjs', sourcemap: true },
     ],
-    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib', include: ['lib/**/*'] })]
+    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib', include: ['lib/**/*'] })],
+    external: ['@kakang/eventemitter', '@kakang/mongodb-aggregate-builder', '@kakang/validator', 'pino', 'uuid']
   },
   {
     input: 'lib/index.ts',
     output: [
       { dir: 'lib/mjs', format: 'esm', sourcemap: true },
     ],
-    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib/mjs', include: ['lib/**/*'] })]
+    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib/mjs', include: ['lib/**/*'] })],
+    external: ['@kakang/eventemitter', '@kakang/mongodb-aggregate-builder', '@kakang/validator', 'pino', 'uuid']
   },
 ]
