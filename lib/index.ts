2e8f459d6acfe6a1da6b65b4afbb7a70b2cab847
KaKa <kaka@kakawebsitedemo.com>
2022-05-15 04:11:39 +0800

    refactor: expose compute shared option

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export { Controller, ControllerOptions, SearchOptions } from './controller/default'
 export { MultiLanguageController, MultiLanguageControllerOptions, MultiLanguageSearchOptions } from './controller/multi-language'
 export * as symbols from './symbols'
+export { computeSharedOption } from './utils/option'
 export { isUpdateQuery, mergeUpdateQueryData, retrieveUpdateQueryData } from './utils/query'
