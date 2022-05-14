56e58229bf6c0bdc160be1f07b13011e23b93091
KaKa <kaka@kakawebsitedemo.com>
2022-05-15 01:09:25 +0800

    refactor: expose search option type

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export { Controller, ControllerOptions } from './controller/default'
-export { MultiLanguageController, MultiLanguageControllerOptions } from './controller/multi-language'
+export { Controller, ControllerOptions, SearchOptions } from './controller/default'
+export { MultiLanguageController, MultiLanguageControllerOptions, MultiLanguageSearchOptions } from './controller/multi-language'
 export * as symbols from './symbols'
 export { isUpdateQuery, mergeUpdateQueryData, retrieveUpdateQueryData } from './utils/query'
